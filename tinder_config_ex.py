fb7bac14afcadca6a49bdef635d1d6bac346214a
havzor1231 <59190208+havzor1231@users.noreply.github.com>
2020-08-25 17:21:50 -0400

    first commit

---
+++ tinder_config_ex.py
@@ -0,0 +1,7 @@
+host = 'https://api.gotinder.com'
+#leave tinder_token empty if you don't use phone verification
+tinder_token = "0bb19e55-5f12-4a23-99df-8e258631105b"
+
+# Your real config file should simply be named "config.py"
+# Just insert your fb_username and fb_password in string format
+# and the fb_auth_token.py module will do the rest!
